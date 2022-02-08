2c6b6dd4b77b450f716265fef67841ac22f8404b
luismany <teacher.luis360@gmail.com>
2022-02-07 22:59:15 -0600

    segundo commit Todo confirmado

---
+++ University.BL/Services/Implements/CarreraService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.BL.Models;
+using University.BL.Repositories;
+
+namespace University.BL.Services.Implements
+{
+    public class CarreraService: GenericService<Carreras>, ICarreraService
+    {
+        public CarreraService(ICarreraRepository carreraRepository):base(carreraRepository)
+        {
+
+        }
+    }
+}
